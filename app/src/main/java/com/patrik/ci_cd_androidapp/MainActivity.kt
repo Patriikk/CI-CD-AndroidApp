4863e86843019b7e4a9accff64d5ecfddb7a66d6
Pratik Fagadiya <pratikf.codezee@gmail.com>
2023-08-31 13:57:36 +0530

    Handle Button Click

--- app/src/main/java/com/patrik/ci_cd_androidapp/MainActivity.kt
+++ app/src/main/java/com/patrik/ci_cd_androidapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.patrik.ci_cd_androidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.patrik.ci_cd_androidapp.databinding.ActivityMainBinding
 
@@ -13,7 +14,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
 
-
+        binding.btnClickMe.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Button clicked", Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
