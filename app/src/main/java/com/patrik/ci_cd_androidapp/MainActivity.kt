2c76addeea58937241ee79ed2db471a094a6a7dd
Pratik Fagadiya <pratikf.codezee@gmail.com>
2023-09-05 09:59:39 +0530

    Refactor

--- app/src/main/java/com/patrik/ci_cd_androidapp/MainActivity.kt
+++ app/src/main/java/com/patrik/ci_cd_androidapp/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
 
+
         binding.btnClickMe.setOnClickListener {
             Toast.makeText(this@MainActivity, "Someone clicked me", Toast.LENGTH_SHORT).show()
         }
